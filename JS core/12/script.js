040fe3d3017b2ae153a802cf65e65f6fceb6103f
bstefanyshyn <108357912+bstefanyshyn@users.noreply.github.com>
2022-10-06 23:50:44 +0300

    part of 12 home work

--- JS core/12/script.js
+++ JS core/12/script.js
@@ -5,7 +5,9 @@ var form = document.querySelector('.form'),
     login = document.querySelector('.login'),
     email = document.querySelector('.email'),
     password = document.querySelector('.password'),
-    submit = document.querySelector('.actionBtn');
+    submit = document.querySelector('.actionBtn'),
+    edit = document.querySelector('.edit'),
+    counter = 0;
 
 // regex configurations
 
@@ -15,112 +17,131 @@ const regexName = /^[A-Za-z]{3,21}$/,
 
 // validate input functions
 
-function validateFirsName(login) {
+function validateLogin(login) {
     return regexName.test(String(login).toLowerCase());
 }
-
 function validatePassword(password) {
     return regexPassword.test(String(password).toLowerCase());
 }
-
 function validateEmail(email) {
     return regexEmail.test(String(email).toLowerCase());
 }
 
-// clases for validate inputs
-
-function FildValid(regExp, fild) {
-
-    if (!regExp.test(fild.value)) {
-        fild.style.cssText = 'border: 3px solid red;';
-        return false;
-    } else {
-        fild.style.cssText = 'border: 3px solid green;';
-        return true;
-    }
-}
-
-// conditions for validate forn
+// conditions for validate form
 
 function checkInputs() {
     let log = login.value,
         passVal = password.value,
         emailVal = email.value;
 
-
-    emptyInputs = Array.from(inputItems).filter(input => input.value === '');
-
     inputItems.forEach(function (input) {
 
         if (input.value === "") {
+            console.log('input not filled');
             input.classList.add('error'); //if input empty => add error style
         }
         else {
             input.classList.remove('error');//if input introduced => remove error style
             input.classList.add('ok');
         }
-    });
-    if (emptyInputs.length !== 0) {
-        console.log('input not filled');
-        return false;
-    }
-    if (!validateFirsName(log)) {
+    })
+    if (!validateLogin(log)) {
         console.log('login not valid');
+        login.classList.remove('ok');
         login.classList.add('error');
         return false;
     }
     else {
-        login.classList.remove('error');
+        login.classList.add('ok');
     }
     if (!validatePassword(passVal)) {
         console.log('password not valid');
+        password.classList.remove('ok');
         password.classList.add('error');
         return false;
     }
     else {
-        password.classList.remove('error');
+        password.classList.add('ok');
     }
     if (!validateEmail(emailVal)) {
         console.log('email not valid');
+        email.classList.remove('ok');
         email.classList.add('error');
         return false;
     }
     else {
-        email.classList.remove('error');
+        email.classList.add('ok');
     }
-
-    form.addEventListener('submit', e => {
-        e.preventDefault(); // error checking
-
-        checkInputs();
-    });
+    counter++;
 }
 
-//Дополнительный совет - не загрязняйте глобальное пространство переменными, лучше объявите функцию, которая будет добавлять вашу разметку в таблицу и объявляйте переменные в ней
-function myFunc() {
+let users = [];
+let index = 0;
 
-    //Для начала, вам нужно найти элемент, в который нужно вставить вашу разметку. Вданном случае таблицу с id='table'.
-    var table = document.querySelector('.table');
-
-    //Тут выдерните ваше значение из locslStorage, я просто присвою 1.
-    var faucet1 = 1;
-    //Теперь создаем строку и присваиваем ее переменной.
-    var tr = document.createElement("tr");
-
-    if (faucet1 == 1) {
-
-        //добавляем разметку в созданную строку
-        tr.innerHTML = `<th>${1} </th> <th>${2}</th> <th> гег</th> <th> гег </th> <th> гег</th> <th> гег</th>`;
-
-        //вставляем строку в таблицу
-        table.appendChild(tr);
-
-    } else {
-
-        console.log('error');
-
+function addUser() {
+    var newUser = {
+        position: counter,
+        name: login.value,
+        pasW: password.value,
+        eAdr: email.value,
+        editBtn: '',
+        deleteBtn: ''
+    };
+    // console.log(Object(newUser));
+    if (validateLogin(login.value) && validatePassword(password.value) && validateEmail(email.value)) {
+        users.push(newUser);
     }
+    console.log(users);
 
+    render();
+}
+
+function render() {
+    var table = document.querySelector('.table'),
+        tr = document.createElement("tr");
+    if (validateLogin(login.value) && validatePassword(password.value) && validateEmail(email.value)) {
+        //add value to tr table
+        tr.innerHTML = `<th>${counter} </th> <th>${users[users.length - 1]['name']}</th> <th> ${users[users.length - 1]['pasW']}</th> <th> ${users[users.length - 1]['eAdr']}</th> <th><button class="edit2">Edit</button></th> <th><button>Delete</button></th>`;
+        //add tr into table
+        table.appendChild(tr);
+    } else {
+        console.log('error');
+    }
+    inputItems.forEach(function (input) {
+        input.value = "";
+        input.classList.remove('ok');
+    });
+    submit.style["boxShadow"] = '0 0 6px green';
 };
 
-myFunc();
+form.addEventListener('submit', e => {
+    e.preventDefault(); // error checking
+
+    checkInputs();
+    addUser();
+
+    inputItems.forEach(function (input) {
+        input.value = "";
+        input.classList.remove('ok');
+    });
+    submit.style["boxShadow"] = '0 0 6px green';
+    let trata = document.querySelector('.edit2');//ПЕРЕЙМЕНУВАТИ
+    trata.addEventListener('click', function () {
+        
+        // users.shift();// ВИДАЛЯЄ ЛИШЕ ПЕРШЕ ЗНАЧЕННЯ
+        console.log(users);
+        // var table = document.querySelector('.table');
+        users.splice(0, 1);
+        // table.deleteRow(1); // НЕ ЗМІНЮЄ ЗНАЧЕННЯ counter
+        render();
+    });
+});
+
+edit.addEventListener('click', function () {
+
+    users.shift();// ВИДАЛЯЄ ЛИШЕ ПЕРШЕ ЗНАЧЕННЯ
+    console.log(users);
+    var table = document.querySelector('.table');
+    table.deleteRow(1); // НЕ ЗМІНЮЄ ЗНАЧЕННЯ counter
+    // render();
+});
